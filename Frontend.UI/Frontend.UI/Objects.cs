a15cacd95e6cd7e0d7e6bb12fdf1f06e29aeded6
Fernando Zúniga <30721618@unitec.edu>
2019-02-25 03:17:19 -0600

    Preparing XML to send to server

--- Frontend.UI/Frontend.UI/Objects.cs
+++ Frontend.UI/Frontend.UI/Objects.cs
@@ -1,23 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 
 namespace Frontend.UI
 {
     public class Objects
     {
-        [XmlRootAttribute("LoginData", Namespace = "http://www.cpandl.com",
-        IsNullable = false)]
+        [XmlRootAttribute("LoginData", IsNullable = false)]
         public class CheckCcPinNumber
         {
             public string CcNumber { get; set; }
             public string PinNumber { get; set; }
         }
-        [XmlRootAttribute("BalanceData", Namespace = "http://www.cpandl.com",
-        IsNullable = false)]
+        [XmlRootAttribute("BalanceData", IsNullable = false)]
         public class CheckBalance
         {
             public string AccountNumber { get; set; }
--- Frontend.UI/Frontend.UI/XmlSerialization.cs
+++ Frontend.UI/Frontend.UI/XmlSerialization.cs
@@ -1,26 +1,27 @@
 ﻿using System;
-using System.Xml;
 using System.Xml.Serialization;
 using System.IO;
-using Frontend.UI;
 
-public class XmlSerialization
+namespace Frontend.UI
 {
-    public static string LoginData (string ccNumber, string pinNumber)
+    public class XmlSerialization
     {
-        Objects.CheckCcPinNumber loginData = new Objects.CheckCcPinNumber();
-        loginData.CcNumber = ccNumber;
-        loginData.PinNumber = pinNumber;
-        XmlSerializer x = new XmlSerializer(loginData.GetType());
-        using(StringWriter textWriter = new StringWriter())
+        public static string LoginData(string ccNumber, string pinNumber)
         {
-            x.Serialize(Console.Out, loginData);
-            Console.WriteLine();
-            Console.ReadLine();
-            x.Serialize(textWriter, loginData);
-            return textWriter.ToString();
+            Objects.CheckCcPinNumber loginData = new Objects.CheckCcPinNumber
+            {
+                CcNumber = ccNumber,
+                PinNumber = pinNumber
+            };
+            XmlSerializer x = new XmlSerializer(loginData.GetType());
+            using (StringWriter textWriter = new StringWriter())
+            {
+                x.Serialize(Console.Out, loginData);
+                Console.WriteLine();
+                Console.ReadLine();
+                x.Serialize(textWriter, loginData);
+                return textWriter.ToString();
+            }
         }
-
-        
     }
 }
